0d11d5b390fcf7e5e87b7fb7e6221d0150ce511b
Greg0109 <greg.rabago@gmail.com>
2021-07-05 18:46:03 +0200

    Fix array giving errors

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,7 @@
 let books = [];
 
-if ("Books" in localStorage) {
+if ('Books' in localStorage) {
+  console.log('Loading from localstorage');
   books = JSON.parse(localStorage.getItem('Books'));
 }
 
@@ -11,7 +12,11 @@ function Book(title, author) {
 
 function newBook(title, author) {
   const newestBook = new Book(title, author);
-  books.push(newestBook);
+  if (books.length > 0) {
+    books.push(newestBook);
+  } else {
+    books = [newestBook];
+  }
   localStorage.setItem('Books', JSON.stringify(books));
 }
 
@@ -27,26 +32,28 @@ function displaybooks() {
   const displayBooks = document.querySelector('.display-books');
   displayBooks.innerHTML = '';
 
-  books.forEach((book) => {
-    const mainDiv = document.createElement('div');
-    mainDiv.classList.add('book');
-
-    const title = document.createElement('h2');
-    title.classList.add('book-title');
-    title.textContent = book.title;
-
-    const author = document.createElement('h3');
-    author.classList.add('book-author');
-    author.textContent = book.author;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('btn-danger');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.addEventListener('click', removebook);
-
-    mainDiv.append(title, author, deleteBtn);
-    displayBooks.appendChild(mainDiv);
-  });
+  if (books.length > 0) {
+    books.forEach((book) => {
+      const mainDiv = document.createElement('div');
+      mainDiv.classList.add('book');
+  
+      const title = document.createElement('h2');
+      title.classList.add('book-title');
+      title.textContent = book.title;
+  
+      const author = document.createElement('h3');
+      author.classList.add('book-author');
+      author.textContent = book.author;
+  
+      const deleteBtn = document.createElement('button');
+      deleteBtn.classList.add('btn-danger');
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.addEventListener('click', removebook);
+  
+      mainDiv.append(title, author, deleteBtn);
+      displayBooks.appendChild(mainDiv);
+    });
+  }
 }
 
 addButton.addEventListener('click', () => {
